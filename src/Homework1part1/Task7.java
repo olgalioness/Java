82387d5a2f70b292be3b895825d7a38efe0533ae
OlgaAstashkina <os.astashkina@yandex.ru>
2023-01-15 22:51:14 +0300

    Homework1path1 Task7

---
+++ src/Homework1part1/Task7.java
@@ -0,0 +1,25 @@
+package Homework1part1;
+
+import java.util.Scanner;
+
+/*
+На вход подается двузначное число n.
+Выведите число, полученное перестановкой цифр в исходном числе n.
+Если после перестановки получается ведущий 0, его также надо вывести.
+Пример входных данных
+45
+Пример выходных данных
+54
+ */
+public class Task7 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        int n = console.nextInt();
+
+        int first = n / 10;
+        int second = n % 10;
+
+        System.out.println(second+""+first);
+    }
+}
+
