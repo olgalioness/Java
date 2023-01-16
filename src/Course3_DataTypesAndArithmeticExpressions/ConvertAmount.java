4e1d742088befc6f4cd22a41b97e8fad0a40da25
OlgaAstashkina <os.astashkina@yandex.ru>
2023-01-16 17:55:02 +0300

    ConvertAmount

--- src/Course3_DataTypesAndArithmeticExpressions/ConvertAmount.java
+++ src/Course3_DataTypesAndArithmeticExpressions/ConvertAmount.java
@@ -3,22 +3,22 @@ package Course3_DataTypesAndArithmeticExpressions;
 import java.util.Scanner;
 
 public class ConvertAmount {
-    public static void main(String[] args){
-        final double ROUBLES_PER_DOLLAR = 72.12;
+    public static void main(String[] args) {
+        final double ROUBLES_PER_EUR = 81.21;
 
-        int dollars;
+        int eur;
         double roubles;
 
         Scanner input = new Scanner(System.in);
 
-        //Получить сумму денег в американских долларах
-        System.out.print("Введите сумму денег в американских долларах: ");
-        dollars = input.nextInt();
+        //Получить сумму денег в евро
+        System.out.print("Введите сумму денег в евро: ");
+        eur = input.nextInt();
 
         //Конвертировать сумму денег в российские рубли
-        roubles = ROUBLES_PER_DOLLAR * dollars;
+        roubles = ROUBLES_PER_EUR * eur;
 
         //Отобразить сумму денег в российских рублях в пользу покупателя
-        System.out.println("Она равна " + (int)(roubles * 100) / 100.0 + " росскийского рубля.");
+        System.out.println("Она равна " + (int) (roubles * 100) / 100.0 + " росскийского рубля.");
     }
 }
