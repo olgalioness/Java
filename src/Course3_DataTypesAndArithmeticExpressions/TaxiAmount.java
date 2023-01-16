d4d4bc95daf65c5bb678cccdad301512401fbb2f
OlgaAstashkina <os.astashkina@yandex.ru>
2023-01-16 15:36:47 +0300

    Course3 added class TaxiAmount

---
+++ src/Course3_DataTypesAndArithmeticExpressions/TaxiAmount.java
@@ -0,0 +1,29 @@
+package Course3_DataTypesAndArithmeticExpressions;
+
+import java.util.Scanner;
+
+public class TaxiAmount {
+
+    public static void main(String[] args) {
+        final double FARE = 8.50;
+
+        int kilometer;
+        int initialReadings;
+        int finalReadings;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Введите начальные показания одометра: ");
+        initialReadings = input.nextInt();
+
+        System.out.print("Введите конечные показания одометра: ");
+        finalReadings = input.nextInt();
+
+        //Конвертировать показания одометра в пройденные км
+        kilometer = finalReadings - initialReadings;
+
+        //Отобразить стоимость проезда
+        System.out.println("Вы проехали " + kilometer + " км." + "Из расчета " + FARE + " руб. за км, стоимость проезда равна " + FARE * kilometer + " руб.");
+    }
+}
+
