d1277446aa5d481524d1fe054aaf30857df1eb3a
OlgaAstashkina <os.astashkina@yandex.ru>
2023-01-22 16:15:40 +0300

    Course3

--- src/Course3_DataTypesAndArithmeticExpressions/ConvertAmount.java
+++ src/Course3_DataTypesAndArithmeticExpressions/ConvertAmount.java
@@ -9,16 +9,16 @@ public class ConvertAmount {
         int eur;
         double roubles;
 
-        Scanner input = new Scanner(System.in);
+        Scanner console = new Scanner(System.in);
 
         //Получить сумму денег в евро
         System.out.print("Введите сумму денег в евро: ");
-        eur = input.nextInt();
+        eur = console.nextInt();
 
         //Конвертировать сумму денег в российские рубли
         roubles = ROUBLES_PER_EUR * eur;
 
         //Отобразить сумму денег в российских рублях в пользу покупателя
-        System.out.println("Она равна " + (int) (roubles * 100) / 100.0 + " росскийского рубля.");
+        System.out.println("Она равна " + (int) (roubles * 100) / 100.0 + " российского рубля.");
     }
 }
