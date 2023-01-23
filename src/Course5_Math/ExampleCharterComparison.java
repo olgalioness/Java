76caf949248af665903684ec15be86caed402071
OlgaAstashkina <os.astashkina@yandex.ru>
2023-01-23 21:28:44 +0300

    Course5

---
+++ src/Course5_Math/ExampleCharterComparison.java
@@ -0,0 +1,38 @@
+package Course5_Math;
+
+public class ExampleCharterComparison {
+    public static void main(String[] args) {
+        char x = 'a';
+        char y = 'c';
+        System.out.println(++x);            //b
+        System.out.println(y++);            //c - выводится знаечение c, после чего к переменной ++
+        System.out.println(x - y);          // b = 98 - d = 100 = -2
+
+        char c = 'A';
+        int i = (int)c;
+        System.out.println(i);              //65
+
+        float f = 1000.34f;
+        i = (int)f;
+        System.out.println(i);              //1000
+
+        double d = 1000.34;
+        i = (int)d;
+        System.out.println(i);              //1000
+
+        int v = 97;
+        char z = (char)v;
+        System.out.println(z);              //a
+
+        System.out.println((int)(Math.random() * 26 + 'a'));            //10
+
+        System.out.println('a' < 'b');          //true
+        System.out.println('a' <= 'A');         //false
+        System.out.println('a' > 'b');          //false
+        System.out.println('a' >= 'A');         //true
+        System.out.println('a' == 'a');         //true
+        System.out.println('a' != 'b');         //true
+    }
+}
+
+
