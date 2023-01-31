48b4e157efaeb02b335a551f35aed133927a2ce9
OlgaAstashkina <os.astashkina@yandex.ru>
2023-01-31 04:24:39 +0300

    Homework1part3 Task1 - Task5

---
+++ src/Homework1part3/Task1.java
@@ -0,0 +1,28 @@
+package Homework1part3;
+/*
+1. Таблица умножения
+Напечатать таблицу умножения от 1 до 9.
+Входных данных нет.
+Многоточие в примере ниже подразумевает вывод таблицы умножения и для остальных чисел 2, 3 и т. д.
+ */
+
+public class Task1 {
+    public static void main(String[] args) {
+        int number1 = 1;
+        int number2;
+        while (number1 <= 9) {
+            number2 = 1;
+            while (number2 <= 9) {
+                System.out.println(number1 + " x " + number2 + " = " + (number1 * number2));
+                number2++;
+            }
+            number1++;
+        }
+    }
+}
+
+
+
+
+
+
---
+++ src/Homework1part3/Task2.java
@@ -0,0 +1,29 @@
+package Homework1part3;
+
+import java.util.Scanner;
+/*
+2. Сумма чисел
+На вход подается два положительных числа m и n.
+Найти сумму чисел между m и n включительно.
+
+Ограничение:
+0 < m, n < 10
+m < n
+ */
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        int m = console.nextInt();
+        int n = console.nextInt();
+
+        if ((0 < m && m < 10) && (0 < n && n < 10) && (m < n)) {
+            int sum = 0;
+            for (int i = m; i <= n; i++) {
+                sum += i;
+            }
+            System.out.println(sum);
+        }
+    }
+}
+
