e5319ff6b147fd37f95ded63e4d90383345fb5f2
OlgaAstashkina <os.astashkina@yandex.ru>
2023-02-03 01:06:14 +0300

    Course6

---
+++ src/Course6_Cycles/MultiplicationTable.java
@@ -0,0 +1,23 @@
+package Course6_Cycles;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        // Отобразить название таблицы
+        System.out.println("\t\t\t\tТАБЛИЦА УМНОЖЕНИЯ\n");
+
+        // Отобразить имена столбцов
+        for (int j = 1; j <= 9; j++)
+            System.out.print("\t" + j);
+
+        System.out.println("\n\t_\t_\t_\t_\t_\t_\t_\t_\t_");
+
+        // Отобразить имена строк и произведение
+        for (int i = 1; i <= 9; i++) {
+            System.out.print(i + "   |");
+            for (int j = 1; j <= 9; j++) {
+                System.out.println("\t" + i * j);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/Course6_Cycles/NumbersPositiveNegative.java
@@ -0,0 +1,36 @@
+package Course6_Cycles;
+
+import java.util.Scanner;
+
+public class NumbersPositiveNegative {
+    public static void main(String[] args) {
+        final int SENTINEL = -99;
+
+        int sum, sum2, score;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Введите положительное или отрицательное число "
+                + SENTINEL + " для выхода: ");
+        score = input.nextInt();
+
+
+        sum = 0;
+        sum2 = 0;
+        while (score != SENTINEL) {
+            if (score >= 0) {
+                sum += 1;
+            } else if (score < 0){
+                sum2 += 1;
+
+            }
+            System.out.print("Введите следующее число "
+                    + SENTINEL + " для выхода: ");
+            score = input.nextInt();
+        }
+
+        System.out.println("Количество введенных положительных чисел " + sum);
+        System.out.println("Количество введенных отрицательных чисел " + sum2);
+    }
+}
+
