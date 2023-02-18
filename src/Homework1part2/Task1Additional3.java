d08a958263b84ade8ed3f78b6dd62c06d895cd42
OlgaAstashkina <os.astashkina@yandex.ru>
2023-02-19 00:52:30 +0300

    Homework1part2 additional3

---
+++ src/Homework1part2/Task1Additional3.java
@@ -0,0 +1,27 @@
+package Homework1part2;
+
+import java.util.Scanner;
+
+public class Task1Additional3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String model = input.nextLine();
+        int price = input.nextInt();
+        String iphone = "iphone";
+        String samsung = "samsung";
+
+        String conculison = model.contains(iphone) || model.contains(samsung) ? ((price >= 50000 && price <= 120000) ? "Можно купить" : "Не подходит") : "Не подходит";
+        System.out.println(conculison);
+
+
+        /*if ((model.contains(iphone) || model.contains(samsung)) && (price >= 50000 && price <= 120000)) {
+                System.out.println("Можно купить");
+            } else {
+                System.out.println("Не подходит");
+            }*/
+        //String conculison = ((model.contains(iphone) || model.contains(samsung)) && (price >= 50000 && price <= 120000)) ? "Можно купить" : "Не подходит";
+
+    }
+}
+
+
