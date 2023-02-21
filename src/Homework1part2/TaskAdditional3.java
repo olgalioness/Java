7b954dc42ff153a2bb989bca830f8289d7293f1d
OlgaAstashkina <os.astashkina@yandex.ru>
2023-02-21 12:08:43 +0300

    renamed

--- src/Homework1part2/TaskAdditional3.java
+++ src/Homework1part2/TaskAdditional3.java
@@ -2,7 +2,7 @@ package Homework1part2;
 
 import java.util.Scanner;
 
-public class Task1Additional3 {
+public class TaskAdditional3 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         String model = input.nextLine();
