eb6e3831277d260473decdcc50ead4c01527cdac
OlgaAstashkina <os.astashkina@yandex.ru>
2023-05-06 19:30:05 +0300

    Spring project

---
+++ SpringLibraryProject/src/test/java/com/jpc16tuesday/springlibraryproject/SpringLibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpc16tuesday.springlibraryproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLibraryProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
